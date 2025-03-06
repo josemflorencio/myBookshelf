7c76a51c028a327ca0759db90c01ebe200bb080a
Jose <josemflorencio09@gmail.com>
2025-03-05 19:06:03 -0500

    get_covers successfully returns and shows book cover from book cover id

--- OpenLibraryAPI.py
+++ OpenLibraryAPI.py
@@ -1,16 +1,17 @@
 import requests
+from PIL import Image
+from io import BytesIO
+
 
 class OpenLibraryAPI:
     baseurl = "https://openlibrary.org/isbn/"
-    def get_book(self, isbn: str):
 
+    def get_book(self, isbn: str):
         info = {}
 
-        print(self.baseurl+".json")
-        response = requests.get(self.baseurl+f"{isbn}.json").json()
+        response = requests.get(self.baseurl + f"{isbn}.json").json()
 
         author_key = response.get('authors')[0].get('key').lstrip('/authors')
-        print(author_key)
 
         info['authors'] = self.get_authors(author_key)
         info['title'] = response.get('title')
@@ -22,9 +23,15 @@ class OpenLibraryAPI:
     def get_authors(self, key: str):
         url = f"https://openlibrary.org/authors/{key}.json"
 
-        print(url)
         response = requests.get(url).json()
 
         return response.get('name')
-    def get_covers(self):
-        return
+
+    def get_covers(self, cover_id: str):
+        url = f"https://covers.openlibrary.org/b/id/{cover_id}-M.jpg"
+        response = requests.get(url)
+
+        img = Image.open(BytesIO(response.content))
+        img.show()
+
+        return response.text
