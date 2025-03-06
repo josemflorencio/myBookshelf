1aa9bec3b732a40c1f987087d401e75c28405f2b
Jose <josemflorencio09@gmail.com>
2025-03-05 19:06:27 -0500

    begin sqlite implementation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from OpenLibraryAPI import OpenLibraryAPI
 
+
 def main():
     print('hello')
+    # Create an instance of the OpenLibraryAPI class
     api = OpenLibraryAPI()
     print(api.get_book("9781649377159"))
     print(api.get_authors("OL7825177A"))
+    print(api.get_covers("14839882"))
 
 if __name__ == "__main__":
     main()
