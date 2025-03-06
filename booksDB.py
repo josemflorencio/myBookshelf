09dea39f259bc0150de23298156e89abb26bdea8
Jose <josemflorencio09@gmail.com>
2025-03-05 19:17:03 -0500

    add_book function

--- booksDB.py
+++ booksDB.py
@@ -6,3 +6,7 @@ class booksDB:
         self.cursor = self.conn.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS books
             (isbn TEXT PRIMARY KEY, title TEXT, author TEXT, cover TEXT)''')
+
+    def add_book(self, book):
+        self.cursor.execute("INSERT INTO books VALUES (?, ?, ?, ?)", (book['isbn'], book['title'], book['authors'], book['cover']))
+        self.conn.commit()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from OpenLibraryAPI import OpenLibraryAPI
+from booksDB import booksDB
 
 
 def main():
@@ -8,6 +9,9 @@ def main():
     print(api.get_book("9781649377159"))
     print(api.get_authors("OL7825177A"))
     print(api.get_covers("14839882"))
+
+    db = booksDB()
+    print(db.add_book(api.get_book("9781649377159")))
 
 if __name__ == "__main__":
     main()
